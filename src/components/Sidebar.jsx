0bb1ba0d41f015e789267af581ff72ab5bc45ec9
manishTerminal <iammanish61@gmail.com>
2024-08-04 12:11:42 +0530

    Added route to home

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,15 +2,18 @@ import subs from "../assets/img/subscriptions.png";
 import home from "../assets/img/home.png";
 import shorts from "../assets/img/shorts.png";
 import next from "../assets/img/next.png";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className=" w-60">
       <div>
+        <Link to="/">
         <button className="flex mx-4 py-2 px-3 w-52 mb-2 rounded-lg bg-slate-100 font-medium">
           <img className="h-6 pr-10" src={home} />
           Home
         </button>
+        </Link>
         <button className="flex mx-4 py-2 px-3 w-52 mb-2 rounded-lg font-medium hover:bg-slate-100">
           <img className="h-6 pr-10" src={shorts} />
           Shorts
